abc39ce627a097e30c7bcad753d75bcb4dfad558
barbara82-r <rviloriabarby23@gmail.com>
2025-07-18 13:54:31 -0400

    tarea

---
+++ Modulo_1/Python/Ejemplos de Python/estudiante.py
@@ -0,0 +1,61 @@
+
+igual = (15 == 15)
+print(f"Igual: {igual}")
+
+
+num1= 1
+num2=5
+num3=13
+
+and_condition=(num3>num2 and num1 < num2)
+
+print(f"and:{and_condition}")
+
+
+nombre = input("cual es tu nombre")
+edad =input("cuantos años tienes")
+origen= input("de donde vienes")
+
+print(f"hola, mi nombre es{nombre }, tengo{edad} años, y soy de{origen } ")
+
+
+x=11
+y=6
+resultado=(x+y)
+print(f"suma:{resultado}")
+
+
+usuario = 5
+
+if usuario == 5:
+    print("acceso concedido")
+else:
+    print("acceso denegado")
+    
+
+calificacion= input("cual es tu calificacion")
+
+if 90-100:
+    print("A")
+
+elif 80-89:
+    print("B")
+
+elif 70-79:
+    print("C")
+
+elif 60-69:
+    print("D")
+    
+elif 0-59:
+    print("F")
+
+else:
+    print("no calificado")
+
+
+
+
+
+
+ 
